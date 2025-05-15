33db7d2a35e4045b32c5ea845d7f138a9a05e8ce
Elli Leppänen <elli.jarvi@hotmail.com>
2025-05-15 14:00:28 +0300

    eka commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def landingview(request):
+    return render(request, 'landingpage.html')
+
+def productlistview(request):
+    return render(request, 'productlist.html')
+
+def supplierlistview(request):
+    # muuttuja = "Tämä on merkkijono"
+    # context = {'x': muuttuja}
+    return render(request, 'supplierlist.html', context)
+
